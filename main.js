cbf2727ed1786792960af4ee4a9f4216cba4db38
Alejandro <miguel2w1q@gmail.com>
2020-08-08 03:37:02 -0500

    game-alpha

--- main.js
+++ main.js
@@ -1,76 +1,145 @@
-var audio = {
-  ready: false,
-  context : null,
-  analyser : null,
-  fftSize: 256,
-  bands: 5,
-  step: null,
-  element : null,
-  bufferLength : null,
-  data: null,
-  track : null,
-  start: function() {
-    this.element = document.getElementById('audio')
-    this.context = new AudioContext()
-    this.analyser = this.context.createAnalyser()
-    this.analyser.fftSize = this.fftSize*2 
-    this.track = this.context.createMediaElementSource(this.element)
-    this.bufferLength =  this.analyser.frequencyBinCount
-    this.step = parseInt(this.bufferLength/this.bands)
-    this.data = new Uint8Array(this.bufferLength)
-    this.track.connect(this.analyser)
-    this.track.connect(this.context.destination)
-    this.element.play()
-    this.ready = true
-  },
-  updateData: function(){
-    this.analyser.getByteFrequencyData(this.data)
-  },
-  getBand: function(number){
-    var result = 0
-    var i = (number-1)*this.step
-    for(i; i< this.step*number; i++)
-      result+= this.data[i]
-    return result/(this.fftSize*this.step)
-  }
+var languague = "es"
+const audio = {
+    bgm: new Audio('audio/bgm.mp3'),
+    click: new Audio('audio/click.mp3'),
+    ending: new Audio('audio/ending.mp3'),
+    typings: [
+        new Audio('audio/t1.mp3'),
+        new Audio('audio/t2.mp3'),
+        new Audio('audio/t3.mp3'),
+        new Audio('audio/t4.mp3'),
+        new Audio('audio/t5.mp3'),
+    ]
+}
+audio.bgm.loop = true
+const speedType = 50
+const endings = [
+    {
+        title: 'BAD ENDING',
+        image: 'img/end1.png',
+        text: 'No lograste escapar de Latinoaméica y todos tus panas fueron víctimas de la ola de crímenes de tu país tercermundista.'
+    },
+    {
+        title: 'NEUTRAL ENDING',
+        image: 'img/end2.png',
+        text: 'Decidiste quedarte en latinoamérica como político corrupto. Tú y tus panas están en la gloria, pero... ¿A qué costo?'
+    },
+    {
+        title: 'FAKE REALITY ENDING',
+        image: 'img/end3.png',
+        text: 'No hay necesidad de escapar de Latinoamérica. Todo a mejorado. No más delicuencia. No más corrupción. Latinoamérica es todoo lo que debió haber sido.'
+    },
+    {
+        title: 'MISSING DEADLINE',
+        image: 'img/end4.png',
+        text: 'Vivir en Latinoamérica es agobiante, pero también lo es la idea de escapar de él. Decidiste no hacer nada. No pensar en nada. Ser  una nada. Nada.'
+    },
+    {
+        title: 'ROYAL ENDING',
+        image: 'img/end5.png',
+        text: 'Mientras tus panas servían como distracción, lograste escapar de Latinoamérica, pero Latinoamérica no ha escapado de ti.'
+    },
+    {
+        title: 'REALLITY ENDING',
+        image: 'img/end6.png',
+        text: 'No debiste salir de Latinoamérica sin saber inglés.'
+    },
+]
+
+function typeText(element, txt){
+    let i = 0
+    typeWriter()
+    function typeWriter(){
+        if (element && i < txt.length) {
+             element.innerHTML += txt.charAt(i)
+             i++
+             audio.typings[Math.round(Math.random()*4)].cloneNode(true).play()
+             setTimeout(typeWriter, speedType)
+           }
+    }
 }
 
-var game = {
-  images: null,
-  body: null,
-  logo: null,
-  start: function(){
-    this.body = document.body
-    this.logo = document.getElementById('logo')
-    this.updateBackground()
-  },
-  updateBackground: function(p1, p2, p3) {
-    this.body.style.background = `radial-gradient(circle, 
-      rgba(255,255,255,0) ${p3}%,
-      rgba(255,0,0,1) ${p2}%,
-      rgba(255,255,255,0) ${p1}%)`
-  }
+function insertAfter(referenceNode, newNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
 }
 
-window.addEventListener('click', function() {
-  if(!audio.ready) audio.start()
-});
-
-window.onload = ()=>{
-  game.start()
-  window.requestAnimationFrame(draw)
+function loadEnding(){
+    let img = document.querySelector('.endingImg')
+    let title = document.querySelector('.endingTitle')
+    let text = document.querySelector('.endingText')
+    text = document.createElement('div')
+    insertAfter(title, text)
+    text.classList.add('endingText')
+    ending = endings[parseInt(Math.random()*(endings.length-1))]
+    img.style.background = `url('${ending.image}')`
+    img.style.backgroundSize = 'cover'
+    img.style.backgroundRepeat = 'no-repeat'
+    img.style.backgroundPosition = 'center'
+    title.innerHTML = ending.title
+    typeText(text, ending.text)
 }
 
-function draw(timestamp){
-  if(audio.ready){
-    audio.updateData()
-    let p1 = audio.getBand(1)
-    let p2 = audio.getBand(2)
-    let p3 = audio.getBand(4)
-    p1 = 100*p1*p1*p1
-    p2 = 100*p2*p2
-    p3 = 100*p3
-    game.updateBackground(p1, p2, p3)
-  }
-  window.requestAnimationFrame(draw)
-}
+document.addEventListener('DOMContentLoaded', ()=>{
+    audio.bgm.volume = 0.3
+    const sGame = new Splide('#game',{
+        type: 'fade',
+        arrows: false,
+        pagination: false,
+        drag: false
+    }).mount()
+
+    new Splide('#background-main',{
+        type: 'fade',
+        rewind: true,
+        speed: 1500,
+        cover: true,
+        heightRatio: 1,
+        arrows: false,
+        pagination: false,
+        drag: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplay: true,
+        interval: 3000,
+        width : '100vw',
+		height: '100vh'
+    }).mount()
+
+    document.querySelector('.load-game').addEventListener('click', ()=>{
+        sGame.go(1)
+        audio.bgm.play()
+    })
+
+    document.querySelector('.start-game').addEventListener('click', ()=>{
+        sGame.go(2)
+        let message = document.createElement('div')
+        message.classList.add('message')
+        let textBox = document.querySelector('.textBox')
+        textBox.insertBefore(message, textBox.firstChild)
+        typeText(message, `Hoshino: ¿Te gusta vivir en latinoamerica?`)
+
+        let hoshino = document.querySelector('.hoshino')
+        hoshino.classList.add('show')
+    })
+
+    document.querySelectorAll('.options button').forEach(button =>{
+        button.addEventListener('click', ()=>{
+            document.querySelector('.message')?.remove()
+            let hoshino = document.querySelector('.hoshino')
+            hoshino.classList.remove('show')
+            sGame.go(3)
+            audio.ending.play()
+            loadEnding()
+        })
+    })
+
+    document.querySelector('.play-again').addEventListener('click', ()=>{
+        document.querySelector('.endingText')?.remove()
+        audio.ending.pause()
+        audio.ending.currentTime = 0
+        sGame.go(1)
+    })
+    document.querySelectorAll('.click').forEach(button =>{
+        button.addEventListener('click', () => audio.click.cloneNode(true).play())
+    })
+})
